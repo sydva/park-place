d7c1eb99aecf53f8ad8af79a45421fa25cb68bd8
Form-And-Function <29594993+Form-And-Function@users.noreply.github.com>
2025-09-06 14:06:42 -0700

    remove home page

--- site/src/App.js
+++ site/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { SignedIn, SignedOut, SignUp, SignIn } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignUp, SignIn, useAuth } from '@clerk/clerk-react';
 import './App.css';
 import Home from './components/Home';
 import Map from './components/Map';
@@ -9,30 +9,51 @@ import AddParkingSpace from './components/AddParkingSpace';
 import ProtectedRoute from './components/ProtectedRoute';
 import CompleteRegistration from './components/CompleteRegistration';
 
+function AppRoutes() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Routes>
+      <Route 
+        path="/" 
+        element={
+          isSignedIn ? (
+            <ProtectedRoute><Map /></ProtectedRoute>
+          ) : (
+            <Home />
+          )
+        } 
+      />
+      <Route 
+        path="/sign-in/*" 
+        element={<SignIn routing="path" path="/sign-in" />} 
+      />
+      <Route 
+        path="/sign-up/*" 
+        element={<SignUp routing="path" path="/sign-up" />} 
+      />
+      <Route path="/complete-registration" element={<CompleteRegistration />} />
+      <Route path="/map" element={<ProtectedRoute><Map /></ProtectedRoute>} />
+      <Route 
+        path="/report-license-plate" 
+        element={<ProtectedRoute><ReportLicensePlate /></ProtectedRoute>} 
+      />
+      <Route 
+        path="/add-parking-space" 
+        element={<ProtectedRoute><AddParkingSpace /></ProtectedRoute>} 
+      />
+    </Routes>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route 
-          path="/sign-in/*" 
-          element={<SignIn routing="path" path="/sign-in" />} 
-        />
-        <Route 
-          path="/sign-up/*" 
-          element={<SignUp routing="path" path="/sign-up" />} 
-        />
-        <Route path="/complete-registration" element={<CompleteRegistration />} />
-        <Route path="/map" element={<ProtectedRoute><Map /></ProtectedRoute>} />
-        <Route 
-          path="/report-license-plate" 
-          element={<ProtectedRoute><ReportLicensePlate /></ProtectedRoute>} 
-        />
-        <Route 
-          path="/add-parking-space" 
-          element={<ProtectedRoute><AddParkingSpace /></ProtectedRoute>} 
-        />
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 }
